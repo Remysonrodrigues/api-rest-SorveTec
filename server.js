9930162e2b7cf4340982171a9afc6a677a201be4
Remyson Costa <remysonrodrigues@gmail.com>
2020-04-27 15:08:01 -0300

    Servidor criado

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+const app = require('./src/app');
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Servidor executando na porta: ${port}`));
---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use( (req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use( (error, req, res, next) => {
+    res.status( error.status || 500);
+    return res.send({
+        mensagem: error.message
+    });
+});
+
+module.exports = app;
+
